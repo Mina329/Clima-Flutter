1bdd740685efa49361e805c5d3eeb8fd81416df5
Mina Emil <minaemil329@gmail.com>
2022-02-18 10:11:26 +0200

    Extract Data from API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -13,11 +14,19 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getlocation();
+    getData();
   }
   void getData() async{
     http.Response response = await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=dd8c8fff906eaeeb9523b32403cbf57b'));
     if(response.statusCode == 200){
       String data =response.body;
+      var decodedData = jsonDecode(data);
+      int temprature = decodedData['main']['temp'];
+      double condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+      print(temprature);
+      print(condition);
+      print(cityName);
     }else{
       print(response.statusCode);
     }
